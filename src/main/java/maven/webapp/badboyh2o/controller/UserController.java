659c7cd2c88c1fba717b5cd3c37442c97a38f37a
badboyh2o <1103299760@qq.com>
2020-12-23 13:44:20 +0800

    s

--- src/main/java/maven/webapp/badboyh2o/controller/UserController.java
+++ src/main/java/maven/webapp/badboyh2o/controller/UserController.java
@@ -8,10 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 
 import maven.webapp.badboyh2o.domain.User;
 import maven.webapp.badboyh2o.service.impl.UserService;
@@ -31,7 +31,7 @@ public class UserController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login(User model, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
-        
+    	log.info("66" + model.toString());
     	User user = userService.findUserByName("11");
     	log.info(user.toString());
     	
